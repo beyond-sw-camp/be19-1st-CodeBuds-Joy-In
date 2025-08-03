5e4736970c703638d31c5d1d2ef16b0b8ca79e9f
suhyun-h <suhyun8913@naver.com>
2025-08-03 23:54:39 +0900

    certification = 1 조건 추가

--- admin/getMatching.sql
+++ admin/getMatching.sql
@@ -1,14 +1,15 @@
 -- ---------------------------------------------------------------------------
 -- 1:1 매칭 조회
 
-SELECT a.id, a.name, a.gender, b.matching_id
-FROM member a
-    JOIN member_matching b ON a.id = b.member_id
-    WHERE b.status = 'Y'
-        AND b.matching_id IN (
-        SELECT matching_id
-        FROM member_matching
-        WHERE status = 'Y'
-        GROUP BY matching_id
-        HAVING COUNT(*) = 2
+
+select a.id, a.name, a.gender, b.matching_id
+from member a
+         join member_matching b on a.id = b.member_id
+where b.status = 'Y' and b.certification = 1
+  and b.matching_id in (
+    select matching_id
+    from member_matching
+    where status = 'Y'
+    group by matching_id
+    having COUNT(*) = 2
 );
