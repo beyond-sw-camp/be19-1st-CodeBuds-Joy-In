5ca259111b642e537182f613ab70474b11cec44e
HyeonggyuKang <kin0204@naver.com>
2025-08-03 14:44:03 +0900

    [기능개발] 리뷰조회
    view 테이블로 생성 id 혹은 이름으로 조회

---
+++ Customer/reviewLookUp.sql
@@ -0,0 +1,24 @@
+
+-- 리뷰 조회 , id 혹은 이름으로
+
+create or replace view reviewLookUp
+as
+select
+       a.name
+     , b.review
+     , b.created_by
+     , a.id
+  from member as a
+join(
+        select
+               review
+             , target_member_id as id
+             , created_by
+          from group_review
+        union all
+        select review
+             , target_member_id  as id
+             , created_by
+          from matching_review
+  ) as b
+on a.id = b.id
