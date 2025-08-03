c08a109d56dbc47c6164c31c4852f7432abb2d29
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-08-03 18:02:52 +0900

    [테스트] 시나리오 수정
    프로시저 변경에 따른 시나리오 쿼리 수정
    프로시저의 out이 추가되어서 수정을 진행

--- DML_Matching/TEST_SCENARIO/S6.sql
+++ DML_Matching/TEST_SCENARIO/S6.sql
@@ -8,26 +8,31 @@ SELECT * FROM member WHERE is_matching_active = 2;
 
 -- 테스트 진행: 위에서 확인한 member의 id로 변경
 SET @is_possible = 0;
+SET @output_text = '';
 -- is_matching_active 가 0인 유저
 SELECT id, is_matching_active FROM member WHERE id = 10005;
-CALL set_matching_active_status(10005, @is_possible);
+CALL set_matching_active_status(10005, @is_possible, @output_text);
 SELECT @is_possible;
+SELECT @output_text;
 SELECT id, is_matching_active FROM member WHERE id = 10005;
 
 -- is_matching_active 가 1인 유저
 SELECT id, is_matching_active FROM member WHERE id = 10004;
-CALL set_matching_active_status(10004, @is_possible);
+CALL set_matching_active_status(10004, @is_possible, @output_text);
 SELECT @is_possible;
+SELECT @output_text;
 SELECT id, is_matching_active FROM member WHERE id = 10004;
 
 -- is_matching_active 가 2인 유저(방금)
 SELECT id, is_matching_active FROM member WHERE id = 10198;
-CALL set_matching_active_status(10198, @is_possible);
+CALL set_matching_active_status(10198, @is_possible, @output_text);
 SELECT @is_possible;
+SELECT @output_text;
 SELECT id, is_matching_active FROM member WHERE id = 10198;
 
 -- is_matching_active 가 2인 유저(30분이 지난)
 SELECT id, is_matching_active FROM member WHERE id = 10181;
-CALL set_matching_active_status(10181, @is_possible);
+CALL set_matching_active_status(10181, @is_possible, @output_text);
 SELECT @is_possible;
+SELECT @output_text;
 SELECT id, is_matching_active FROM member WHERE id = 10181;
