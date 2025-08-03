55acad06c3bad1fc050c64a6dce569a2823c11ce
suhyun-h <suhyun8913@naver.com>
2025-08-03 23:53:54 +0900

    certification = 1 조건 추가

--- admin/getGroupMatching.sql
+++ admin/getGroupMatching.sql
@@ -3,6 +3,6 @@
 select a.id, a.name, a.gender, b.post_id, c.title
 from member a join join_request b on a.id = b.member_id
               join post c
-where b. status = 'Y' and c.id = b.post_id and b.post_id in
-                        (select post_id from join_request
-                            where status = 'Y' and certification = 1 group by post_id);
+where b. status = 'Y' and c.id = b.post_id and b.certification = 1 and b.post_id in
+                                                                       (select post_id from join_request
+                                                                        where status = 'Y' group by post_id);
