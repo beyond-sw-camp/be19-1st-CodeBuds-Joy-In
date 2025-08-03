39bd7f93f4e00a704d24b3f1ad18e2f8c6add3af
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-08-03 12:07:37 +0900

    [기능개발] 일대일 매칭 선정 프로시저
    일대일 매칭 기능이 활성화인 사람 두명을 뽑아서 제안하는 프로시저
    매칭 제안 시 해당 회원들의 매칭 활성화를 나타내는 컬럼 is_matching_active는 0으로 비활성화된다.
    현재는 단순히 나열해서 두 명을 순서대로 뽑는 방식이므로 추후 취향을 통ㅇ한 선정이 가능하도록 추가 개발이 필요하다.

---
+++ DDL_Matching/PROCEDURE/matching_user_to_user.sql
@@ -0,0 +1,59 @@
+DROP PROCEDURE IF EXISTS matching_user_to_user;          -- 매칭 제안
+
+
+DELIMITER //
+
+-- 유저에게 매칭을 제안하는 프로시저
+CREATE PROCEDURE matching_user_to_user(
+    IN user_index          INT,
+	 OUT out_user1_id       INT,
+	 OUT out_user2_id       INT,
+	 OUT matching_code      INT
+)
+
+BEGIN
+
+   DECLARE first_user_id   INT;
+   DECLARE second_user_id  INT;
+   DECLARE next_index      INT;
+       SET next_index = user_index + 1;
+   -- 첫 번째 유저 id
+    SELECT 
+	        id INTO first_user_id
+      FROM (
+            SELECT 
+				       id 
+				  FROM member
+			    WHERE is_matching_active = 1
+				 ORDER BY id
+				 LIMIT user_index, 1
+			  ) AS sub1;
+			   
+   -- 두  번째 유저 id
+    SELECT id INTO second_user_id
+      FROM (
+            SELECT id 
+				  FROM member
+			    WHERE is_matching_active = 1
+			    ORDER BY id
+		       LIMIT next_index, 1
+			  ) AS sub2;
+			  
+	    SET out_user1_id = first_user_id;
+	    SET out_user2_id = second_user_id;
+	    
+	 -- 매칭 기능을 비활성화 -> 수락 거절 전에 다시 매칭하면 안되니까
+	 UPDATE member
+	    SET is_matching_active = 0
+	  WHERE id IN (first_user_id, second_user_id);
+	  
+	 INSERT INTO matching (STATUS, start_date) 
+	 VALUES (0, NOW());
+	  
+	    -- 세션 기준으로 마지막에 삽입된 id 
+	    -- SELECT로 단순 정렬할 경우 동시성 문제로 에러가 날 확률이 존재함
+	    SET matching_code = LAST_INSERT_ID();
+
+END //
+
+DELIMITER ;
